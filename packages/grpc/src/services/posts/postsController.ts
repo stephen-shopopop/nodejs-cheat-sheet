574799c5fc0016f80ae9d81a27052c7e8fc8f2e0
stephen-shopopop <94382341+stephen-shopopop@users.noreply.github.com>
2022-08-19 19:01:25 +0200

    feat(grpc): FIX

--- packages/grpc/src/services/posts/postsController.ts
+++ packages/grpc/src/services/posts/postsController.ts
@@ -43,6 +43,6 @@ export async function handleEditPosts (request: FastifyRequest, _reply: FastifyR
 }
 
 /** Create post */
-export async function handleAddPosts (request: FastifyRequest, _reply: FastifyReply): Promise<{} | undefined | null> {
+export async function handleAddPosts (request: FastifyRequest, reply: FastifyReply): Promise<Posts | null | undefined> {
   return await addPostsClient(request.body as Posts)
 }
